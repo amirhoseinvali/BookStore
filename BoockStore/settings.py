84082a3c32120f32274d065c9e84e8c8f776c0e7
amirhosein vali <amirhosein.vali@yahoo.com>
2024-04-10 16:21:44 +0330

    change database to sqlite for test

--- BoockStore/settings.py
+++ BoockStore/settings.py
@@ -72,17 +72,25 @@ TEMPLATES = [
 WSGI_APPLICATION = 'BoockStore.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'bookstore',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': 'localhost',
+#         'PORT': '5432', # default PostgreSQL port
+#     }
+# }
+
+
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bookstore',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432', # default PostgreSQL port
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR + '/db.sqlite3',
     }
 }
 
