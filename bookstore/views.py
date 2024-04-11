3d6d97469361090b4f579507df6ade8c9a77e10d
amirhosein vali <amirhosein.vali@yahoo.com>
2024-04-11 16:11:55 +0330

    add postman collection

--- bookstore/views.py
+++ bookstore/views.py
@@ -1,8 +1,5 @@
-from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from rest_framework_simplejwt import authentication
 from rest_framework.permissions import IsAuthenticated
-from django.contrib.auth.models import Permission
 from rest_framework.decorators import  permission_classes
 from django.http import JsonResponse
 from .models import Books, Category, UserInventory, ChargeTokens, BookOrders
