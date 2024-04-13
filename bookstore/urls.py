bf506be44e9ab32a71e38187d235c36c3eb60699
AmirhoseinVali <amirhosein.vali@yahoo.com>
2024-04-13 18:22:19 +0330

    change api view to class based generics

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -4,14 +4,14 @@ from . import views
 
 
 urlpatterns = [
-    path('books_list', views.books_list),
-    path('category_list', views.category_list),
-    path('charge_request/', views.charge_request),
-    path('charge/', views.charge),
-    path('buy_book/', views.buy_book),
-    path('reverse_order/', views.reverse_order),
-    path('user_books', views.user_books),
-    path('user_orders', views.user_orders),
+    path('books_list', views.BooksList.as_view()),
+    path('category_list', views.Category.as_view()),
+    path('charge_request/', views.ChargeRequest.as_view()),
+    path('charge/', views.Charge.as_view()),
+    path('buy_book/', views.Buy.as_view()),
+    # path('reverse_order/', views.reverse_order),
+    # path('user_books', views.user_books),
+    # path('user_orders', views.user_orders),
 
 
 ]
